c77ab825e7469111dc50d4f3e8fca658ffb32d9b
Sivram Pradhan <programmersiv21@gmail.com>
2024-03-12 19:52:06 +0530

    Update App.js

--- 07_project/src/App.js
+++ 07_project/src/App.js
@@ -8,7 +8,6 @@ import Dashboard from "./pages/Dashboard"
 import { useEffect, useState } from 'react'
 import PrivateRoute from "./components/PrivateRoute";
 
-
 function App() {
 
   const [isLoggedIn, setIsLoggedIn] = useState(false);
