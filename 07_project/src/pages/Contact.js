7e872d477367b2757934a184b39dfec72e8b1007
Sivram Pradhan <programmersiv21@gmail.com>
2024-03-19 19:26:12 +0530

    changes

--- 07_project/src/pages/Contact.js
+++ 07_project/src/pages/Contact.js
@@ -6,21 +6,25 @@ import service from '../assets/service.jpeg'
 
 const Contact = () =>{
 return(
-    <div>
-        <h1 className="text-richblack-100 text-3xl text-center">Get Assistance</h1>
-        <div className="flex flex-row gap-4 justify-center mt-5">
+    <div className="mt-[40px] w-100">
+        <div className="flex flex-row gap-4 justify-center mt-5 grow-0">
             <div>
                 <img src={contact} className="w-[350px] h-60"/>
-                <h1 className="text-richblack-100 text-center text-2xl mt-5 ">Need help with your hardware <br/>or order?</h1>
-                <h2 className="text-richblack-100 text-center text-xl mt-5">Find answers about your Peloton hardware,<br/> check your order status and more.</h2>
+                <h1 className="text-richblack-100 text-center text-2xl mt-5">Need help with our service <br/>or order?</h1>
+                <h2 className="text-richblack-100 text-center text-xl mt-5 mb-3">Find answers about our services,<br/> check your order status and more.</h2>
                 <button className="text-white bg-red p-3 px-[95px] bg-red-600 font-medium">CHAT WITH SUPPORT</button>
-                {/* <button className="text-black bg-white p-3 px-[95px] font-medium">TALK TO OUR EXECUTIVE</button> */}
+                <div><br/></div>
+                <button className="text-black bg-white p-3 px-[81px] font-medium">TALK TO OUR EXECUTIVE</button>
             </div>
+            <div>
                 <img src={service} className="w-[350px] h-60"/>
-                <h1></h1>
-                <h2></h2>
-                <button></button>
-            <div></div>
+                <h1 className="text-richblack-100 text-center text-2xl mt-5">Need help with our service <br/>or order?</h1>
+                <h2 className="text-richblack-100 text-center text-xl mt-5 mb-3">Find answers about our services,<br/> check your order status and more.</h2>
+                <button className="text-white bg-red p-3 px-[95px] bg-red-600 font-medium">CHAT WITH SUPPORT</button>
+                <div><br/></div>
+                <button className="text-black bg-white p-3 px-[81px] font-medium">TALK TO OUR EXECUTIVE</button>
+
+            </div>
         </div>
     </div>
 );
