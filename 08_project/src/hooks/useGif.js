fa4d928853ea3cae51b84e4942c9d890c4f98c6b
Sivram Pradhan <programmersiv21@gmail.com>
2024-04-16 01:16:45 +0530

    changes

--- 08_project/src/hooks/useGif.js
+++ 08_project/src/hooks/useGif.js
@@ -3,7 +3,6 @@ import axios from 'axios';
 import { useEffect } from 'react';
 import { useState } from 'react';
 
-
 const API_KEY = process.env.REACT_APP_GIPHY_API_KEY;
 const url = `https://api.giphy.com/v1/gifs/random?api_key=${API_KEY}`;
 
